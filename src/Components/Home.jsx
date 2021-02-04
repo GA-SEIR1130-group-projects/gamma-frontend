948456e131d40dd3864703b4b58aadf70bad1f9f
jephrae <jeff.simons@gmail.com>
2021-02-04 11:43:05 -0600

    trying to get api

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const url = "https://gamma-finsta.herokuapp.com/api/users";
 
 export default function Home() {
+  const [image, setImage] = useState([]);
+
+  const getImage = async () => {
+    const response = await fetch(url);
+    const image = await response.json();
+    setImage(image);
+  };
+  useEffect(() => {
+    getImage();
+  }, []);
+
+  const imageData = image.results;
+
+  if (image.length < 1) {
+    return <h3> loading....</h3>;
+  }
+
   return (
-    <div>
+    <div className="HomeHeader">
       <h1>home test</h1>
+      <div className="ImageContainer">
+        {imageData.map((image, index) => {
+          return `username: ${image.username}`;
+        })}
+      </div>
     </div>
   );
 }
