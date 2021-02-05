a5f25becb06198a009f2558f3e8e5341f3abf1f7
jephrae <jeff.simons@gmail.com>
2021-02-05 14:29:47 -0600

    added buttons, fields on profile page

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
 
 const axios = require("axios");
 
@@ -18,15 +23,47 @@ function UserProfile(props) {
   if (!userProfile) {
     return <h3>loading...</h3>;
   }
+  if (!userProfile.name) {
+    userProfile.name = " unknown";
+  }
+  if (!userProfile.location) {
+    userProfile.location = " parts unkown";
+  }
 
   return (
     <div className="profile-holder">
-      <div className="username">
-        <h1>{userProfile.username}</h1>
-      </div>
-      <div className="user-desc">
-        <h3>{userProfile.desc}</h3>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <div className="username">
+              <h2>{userProfile.username}</h2>
+            </div>
+            <div className="user-name">name:{userProfile.name}</div>
+            <div className="user-location">
+              location: {userProfile.location}
+            </div>
+            <div className="user-desc">
+              <p>description: {userProfile.desc}</p>
+            </div>
+          </Col>
+          <Col>
+            <div className="user-buttons">
+              <Form>
+                <Form.Control placeholder="username"></Form.Control>
+                <Button variant="primary"> Update Name</Button>
+              </Form>
+              <Form>
+                <Form.Control placeholder="location"></Form.Control>
+                <Button variant="primary"> Update Location</Button>
+              </Form>
+              <Form>
+                <Form.Control placeholder="description"></Form.Control>
+                <Button variant="primary"> Update Description</Button>
+              </Form>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
