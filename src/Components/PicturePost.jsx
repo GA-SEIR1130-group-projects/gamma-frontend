1f2e0362ffad323c6a851b418bc32004f8bc5e2e
biprajit163 <biprajit163@gmail.com>
2021-02-05 15:53:19 -0500

    post comments is functional

--- src/Components/PicturePost.jsx
+++ src/Components/PicturePost.jsx
@@ -4,7 +4,8 @@ import { useState, useEffect } from 'react';
 
 function PicturePost() {
     
-    const [comments, setComments] = useState([])
+    const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState("")
 
     function addComment(comment) {
         const commentsArr= [...comments, comment];
@@ -15,40 +16,44 @@ function PicturePost() {
 
     function handleSubmit(event) {
         event.preventDefault();
+        addComment(newComment);
+        setNewComment("");
     }
 
-    function handleChange() {
-
+    function handleChange(event) {
+        setNewComment(event.target.value);
     }
 
     return(
         <div className="picture-post">
             <h1>Post Component goes here</h1>
+
             <div className="picture-section">
                 <img src="" alt="Post Picture"/>
             </div>
 
-            <div className="comment-section">
-                <div className="comment-list">
-                    {
-                        comments.map(comment => {
-                            return(
-                                <div class="user-comment">
-                                    <p>user: {comment}</p>
-                                </div>
-                            );
-                        })
-                    }
-                </div>
-                <form className="add-comment" action="submit">
-                    <label>Comment:</label>
-                    <input 
-                        type="text"
-                        value="comment goes here"
-                    />
-                    <button type="submit">post</button>
-                </form>
+            <div className="comment-list">
+                {
+                    comments.map(comment => {
+                        return(
+                            <div class="user-comment">
+                                <p>user: {comment}</p>
+                            </div>
+                        );
+                    })
+                }
             </div>
+
+            <form className="add-comment" onSubmit={handleSubmit}>
+                <label>Comment:</label>
+                <input
+                    placeholder="Add a comment..." 
+                    type="text"
+                    value={newComment}
+                    onChange={handleChange}
+                />
+                <button type="submit">post</button>
+            </form>
         </div>
     );
 }
