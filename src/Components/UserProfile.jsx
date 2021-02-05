0ed5da432232996e2634bafa8a4fb736fd1a70a5
jephrae <jeff.simons@gmail.com>
2021-02-05 10:14:39 -0600

    userProfile returning username and desc

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -1,9 +1,29 @@
 import React, { useState, useEffect } from "react";
 
-function UserProfile() {
+function UserProfile(props) {
+  const [userProfile, setUserProfile] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://finsta-v2.herokuapp.com/api/users/${props.match.params.id}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setUserProfile(res);
+      })
+      .catch(console.error);
+  }, [props.match.params.id]);
+
+  if (!userProfile) {
+    return <h3>loading...</h3>;
+  }
+
   return (
-    <div>
-      <h3>test</h3>
+    <div className="profile-holder">
+      <div className="username">
+        <h1>{userProfile.username}</h1>
+      </div>
+      <div className="user-desc">
+        <h3>{userProfile.desc}</h3>
+      </div>
     </div>
   );
 }
