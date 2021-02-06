a96b64b0dab433cf5d8373f34fff579752e0fc12
biprajit163 <biprajit163@gmail.com>
2021-02-06 01:43:23 -0500

    work on Post component

--- src/Components/PicturePost.jsx
+++ src/Components/PicturePost.jsx
@@ -1,18 +1,50 @@
 import "../Styling/postStyle.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 import React from 'react';
 import { useState, useEffect } from 'react';
 
+const axios = require("axios");
+
+
+
 function PicturePost() {
     
-    const [comments, setComments] = useState([]);
+    const url = "http://localhost:2000/api/comments";
+    
+    
     const [newComment, setNewComment] = useState("")
+    const [comments, setComments] = useState({
+        comments: []
+    });
+
+    useEffect(() => {
+        getComments()
+    }, []);
+
+    function getComments() {
+        axios.get(url)
+            .then(res => {
+                console.log(res);
+                setComments({ comments: res.data })
+            })
+    }
 
     function addComment(comment) {
-        const commentsArr= [...comments, comment];
-        setComments(commentsArr);
+        axios.post(url, {
+            comment: comment
+        })
+        getComments()
     }
-    
-    useEffect(() => {}, []);
+
+    function deleteComment(id) {
+        axios.delete(`${url}/${id}`);
+        getComments()
+    }
+
+    function updateComment(id, val) {
+        axios.put(`${url}/${id}`, { comment: val })
+            .then(() => getComments());
+    }
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -24,6 +56,7 @@ function PicturePost() {
         setNewComment(event.target.value);
     }
 
+
     return(
         <div className="picture-post">
             <h1>Post Component goes here</h1>
@@ -34,10 +67,21 @@ function PicturePost() {
 
             <div className="comment-list">
                 {
-                    comments.map(comment => {
+                    comments.comments.map(comment => {
                         return(
-                            <div class="user-comment">
-                                <p>user: {comment}</p>
+                            <div 
+                                class="user-comment" 
+                                onClick={() => deleteComment(comment._id)}
+                            >
+                                <p>user: {comment.comment}</p>
+                                <button
+                                    type="button" class="btn btn-danger"
+                                    onClick={() => deleteComment(comment._id)}
+                                >Delete</button>
+                                <button 
+                                    type="button" class="btn btn-info"
+                                    onClick={() => updateComment(comment._id, "updated comment")}
+                                >Update</button>
                             </div>
                         );
                     })
