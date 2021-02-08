41ed1da2b4180d179423ce0a837a9f02061cd208
jephrae <jeff.simons@gmail.com>
2021-02-08 16:09:55 -0600

    firstname put workin

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -9,77 +9,105 @@ import FormControl from "react-bootstrap/FormControl";
 const axios = require("axios");
 
 function UserProfile({ profile }) {
-  
   const [userProfile, setUserProfile] = useState({
     firstname: "",
     lastname: "",
     username: "",
     password: "",
     desc: "",
-    images: []
+    images: [],
   });
 
   const initialImage = {
-    url: ""
-  }  
+    url: "",
+  };
+  const initialName = {
+    firstname: "",
+  };
+  const [name, setName] = useState(initialName);
   const [image, setImage] = useState(initialImage);
 
   useEffect(() => {
     let userId = 0;
-    
-    if(localStorage.getItem("user-id")) {
-      userId = JSON.parse(localStorage.getItem("user-id"))
+
+    if (localStorage.getItem("user-id")) {
+      userId = JSON.parse(localStorage.getItem("user-id"));
     }
 
-    axios.get(`https://finsta-v2.herokuapp.com/api/users/${userId}`)
-      .then(res => {
-        console.log(res.data)
+    axios
+      .get(`https://finsta-v2.herokuapp.com/api/users/${userId}`)
+      .then((res) => {
+        console.log(res.data);
         setUserProfile({
           firstname: res.data.firstname,
           lastname: res.data.lastname,
           username: res.data.username,
           password: res.data.password,
           desc: res.data.desc,
-          images: res.data.images
+          images: res.data.images,
         });
-
-      })
-
-  }, [])
-
-  const imageSubmit = e => {
+      });
+  }, []);
+  //////////// image submit
+  const imageSubmit = (e) => {
     e.preventDefault();
 
     let userId = 0;
-    if(localStorage.getItem("user-id")) {
-      userId = JSON.parse(localStorage.getItem("user-id"))
+    if (localStorage.getItem("user-id")) {
+      userId = JSON.parse(localStorage.getItem("user-id"));
     }
 
-    let allImages = userProfile.images.push(image)
-    axios.put(`https://finsta-v2.herokuapp.com/api/users/${userId}/images`, image)
+    let allImages = userProfile.images.push(image);
+    axios.put(
+      `https://finsta-v2.herokuapp.com/api/users/${userId}/images`,
+      image
+    );
 
-    setImage(initialImage)
-  }
+    setImage(initialImage);
+  };
 
   function imageChange(event) {
     const userInput = event.target.value;
-    setImage(prevState => {
-      return({
+    setImage((prevState) => {
+      return {
         ...prevState,
-        url: userInput
-      })
-    })
+        url: userInput,
+      };
+    });
   }
+  //////////////////////// firstname Update
+  const nameSubmit = (e) => {
+    e.preventDefault();
 
+    let userId = 0;
+    if (localStorage.getItem("user-id")) {
+      userId = JSON.parse(localStorage.getItem("user-id"));
+    }
+
+    let theName = userProfile.images.push(name);
+    axios.put(`https://finsta-v2.herokuapp.com/api/users/${userId}/`, name);
+
+    setName(initialName);
+  };
+
+  function nameChange(event) {
+    const userInput = event.target.value;
+    setName((prevState) => {
+      return {
+        ...prevState,
+        firstname: userInput,
+      };
+    });
+  }
+  /////////////////
 
   return (
     <div className="profile-holder">
-      
       <Container>
         <Row>
           <Col>
             <div className="username">
-              <h2>{userProfile.username}</h2>
+              <h1>{userProfile.username}</h1>
             </div>
             <div className="user-name">name:{userProfile.firstname}</div>
             <div className="user-location">
@@ -91,20 +119,26 @@ function UserProfile({ profile }) {
           </Col>
           <Col>
             <div className="user-buttons">
-              <Form inline>
-                <Form.Control
-                  placeholder="username"
-                  id="inlineFormInputUsername"
-                ></Form.Control>
-                <Button variant="primary"> Update Name</Button>
+              <Form
+                inline
+                placeholder="username"
+                id="inlineFormInputUsername"
+                // value={userProfile.firstname}
+                onChange={nameChange}
+              >
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="first name"
+                  value={name.firstname}
+                  onChange={nameChange}
+                />
+                <Button variant="primary" type="submit" onClick={nameSubmit}>
+                  {" "}
+                  Update Name
+                </Button>
               </Form>
-              <Form inline>
-                <Form.Control
-                  placeholder="location"
-                  id="inlineFormInputLocation"
-                ></Form.Control>
-                <Button variant="primary"> Update Location</Button>
-              </Form>
+
               <Form inline>
                 <Form.Control
                   placeholder="description"
@@ -116,41 +150,44 @@ function UserProfile({ profile }) {
           </Col>
         </Row>
 
-        <button type="button" class="btn btn-dark float-right">Add Picture</button>
-
-        <form class="form-group">
-          <input 
-            type="text" 
-            className="form-control" 
-            placeholder="image url"
-            value={image.url}
-            onChange={imageChange}
-          />
-          <button 
-            type="submit" 
-            className="btn btn-primary"
-            onClick={imageSubmit}
-          >submit pic</button>
-        </form>
-
+        <button type="button" class="btn btn-dark float-right">
+          Add Picture
+        </button>
         <Row>
+          <br />
+          <div className="url-submit-form">
+            <form className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="image url"
+                value={image.url}
+                onChange={imageChange}
+              />
+              <button
+                type="submit"
+                className="btn btn-primary"
+                onClick={imageSubmit}
+              >
+                submit pic
+              </button>
+            </form>
+          </div>
+        </Row>
+
+        {/* <Row>
           {" "}
           <h1> password reset placeholder</h1>
-        </Row>
+        </Row> */}
       </Container>
 
       <Container className="user-pictures">
-        {
-          userProfile.images ?
-          userProfile.images.map(image => {
-            return(
-              <div className="image">{image.url}</div>
-            );
-          }): 
-          null
-        }
+        {userProfile.images
+          ? userProfile.images.map((image) => {
+              return <div className="image">{image.url}</div>;
+            })
+          : null}
       </Container>
-     
     </div>
   );
 }
