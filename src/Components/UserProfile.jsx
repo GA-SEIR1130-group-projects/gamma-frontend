c62329dc0d983dac13a0a8b28c622f029c921c96
biprajit163 <biprajit163@gmail.com>
2021-02-08 15:51:06 -0500

    commit

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -71,73 +71,100 @@ function UserProfile({ profile }) {
     })
   }
 
+  function logout() {
+    localStorage.clear();
+    window.location.reload();
+  }
 
   return (
     <div className="profile-holder">
-      
-      <Container>
-        <Row>
-          <Col>
-            <div className="username">
-              <h2>{userProfile.username}</h2>
-            </div>
-            <div className="user-name">name:{userProfile.firstname}</div>
-            <div className="user-location">
-              {/* location: {userProfile.location} */}
-            </div>
-            <div className="user-desc">
-              <p>description: {userProfile.desc}</p>
-            </div>
-          </Col>
-          <Col>
-            <div className="user-buttons">
-              <Form inline>
-                <Form.Control
-                  placeholder="username"
-                  id="inlineFormInputUsername"
-                ></Form.Control>
-                <Button variant="primary"> Update Name</Button>
-              </Form>
-              <Form inline>
-                <Form.Control
-                  placeholder="location"
-                  id="inlineFormInputLocation"
-                ></Form.Control>
-                <Button variant="primary"> Update Location</Button>
-              </Form>
-              <Form inline>
-                <Form.Control
-                  placeholder="description"
-                  id="inlineFormDescLocation"
-                ></Form.Control>
-                <Button variant="primary"> Update Description</Button>
-              </Form>
-            </div>
-          </Col>
-        </Row>
+      {
+        localStorage.getItem("user-id") === null ?
+        null :
+        <>
+          <div className="btn-group float-right" role="group" aria-label="Basic example">
+            <button type="button" className="btn btn-secondary">Profile</button>
+            <button type="button" className="btn btn-secondary">Edit Profile</button>
+            <button type="button" className="btn btn-secondary">Change Password</button>
+            <button 
+              type="button" 
+              className="btn btn-outline-dark"
+              onClick={logout}
+            >
+              Logout
+            </button>
+          </div>
+          <Container>
+            <Row>
+              <Col>
+                <div className="username">
+                  <h2>{userProfile.username}</h2>
+                </div>
+                <div className="user-name">name:{userProfile.firstname}</div>
+                <div className="user-location">
+                  {/* location: {userProfile.location} */}
+                </div>
+                <div className="user-desc">
+                  <p>description: {userProfile.desc}</p>
+                </div>
+              </Col>
+              <Col>
+                <div className="user-buttons">
+                  <Form inline>
+                    <Form.Control
+                      placeholder="username"
+                      id="inlineFormInputUsername"
+                    ></Form.Control>
+                    <Button variant="primary"> Update Name</Button>
+                  </Form>
+                  <Form inline>
+                    <Form.Control
+                      placeholder="location"
+                      id="inlineFormInputLocation"
+                    ></Form.Control>
+                    <Button variant="primary"> Update Location</Button>
+                  </Form>
+                  <Form inline>
+                    <Form.Control
+                      placeholder="description"
+                      id="inlineFormDescLocation"
+                    ></Form.Control>
+                    <Button variant="primary"> Update Description</Button>
+                  </Form>
+                </div>
+              </Col>
+            </Row>
 
-        <button type="button" class="btn btn-dark float-right">Add Picture</button>
+            <button type="button" class="btn btn-dark float-right">Add Picture</button>
 
-        <form class="form-group">
-          <input 
-            type="text" 
-            className="form-control" 
-            placeholder="image url"
-            value={image.url}
-            onChange={imageChange}
-          />
-          <button 
-            type="submit" 
-            className="btn btn-primary"
-            onClick={imageSubmit}
-          >submit pic</button>
-        </form>
+            <form class="form-group">
+              <div class="input-group mb-3">
+                <input 
+                  type="text" 
+                  className="form-control" 
+                  placeholder="image url"
+                  value={image.url}
+                  onChange={imageChange}
+                />
+                <div className="input-group-append">
+                  <button 
+                    type="submit" 
+                    className="btn btn-primary"
+                    onClick={imageSubmit}
+                  >
+                    submit pic
+                  </button>
+                </div>
+              </div>
+            </form>
 
-        <Row>
-          {" "}
-          <h1> password reset placeholder</h1>
-        </Row>
-      </Container>
+            <Row>
+              {" "}
+              <h1> password reset placeholder</h1>
+            </Row>
+          </Container>
+        </>
+      }
 
       <Container className="user-pictures">
         {
--- src/Components/login/Log.jsx
+++ src/Components/login/Log.jsx
@@ -96,11 +96,14 @@ export default function Header() {
                                 onChange={handlePassword}
                         />
                     </form>
+                    <Link to={"/userprofile"}>
+                        Login
+                    </Link>
                     <button
                         className="d-flex justify-content-center"
                         type="submit"
                         onClick={Signin}
-                    > <Link to="/userprofile">Login</Link>
+                    > Login
                     </button>
                     Not a member? <Link to={"/Register"} className="text-warning">
                         Register here
