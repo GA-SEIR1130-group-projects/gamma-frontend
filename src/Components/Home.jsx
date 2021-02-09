6e55df7abe38bd6199164716ab1e9798ee6bd962
jephrae <jeff.simons@gmail.com>
2021-02-09 13:22:25 -0600

    random images showing, need put them in cards

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
-import '../Styling/Images.css'
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
+import "../Styling/Images.css";
 
 export default function Home() {
   const [users, setUsers] = useState([]);
@@ -13,41 +15,77 @@ export default function Home() {
       .catch(console.error);
   }, []);
 
-  return (
-    <div className="holder">
-      <h3>home test</h3>
-      
-      {users.map((user) => {
-        
-      
-{
-  
-          if (user.images[0] != undefined){
-            const img2= `https://finsta-v2.herokuapp.com/api/users${user.images[0]}`
-            console.log(img2)
-            console.log(user.images[0].url) 
-            console.log(user)
-            
-            return(<div id = 'ImageContainer'> 
-             <img src={user.images[0].url} alt='' class='Image'/>
-             </div>)
-          }
-        }
+  return users.map((item) => {
+    if (item.images.length) {
+      let randIndex = Math.floor(Math.random() * item.images.length);
 
-        
-      })}
-    </div>
-  );
+      if (item.images.length < 5) {
+        for (let i = 0; i < item.images.length; i++) {
+          return (
+            <div>
+              <img src={item.images[randIndex].url} />
+            </div>
+          );
+        }
+      } else {
+        for (let i = 0; i < 5; i++) {
+          return (
+            <div>
+              <img src={item.images[randIndex].url} />
+            </div>
+          );
+        }
+      }
+    }
+  });
 }
 
- 
- /* we need three if statemts
- user.map
- for(let i =0; i <5; i++){
-   (item =>{
+/* we need three if statemts
+  user.map
+  for(let i =0; i <5; i++){
+    (item =>{
+      
+    })
+  }
+  
+  */
 
-   })
- }
- 
- */
- 
+// <div className="holder">
+//   <h3>home test</h3>
+//   {console.log(users)}
+//   {users.map((user) => {
+//     if (user.images.length) {
+//       for (let i = 0; i < 5; i++) {
+//         return (
+//           <div className="image">
+//             <CardDeck style={{ width: "16rem" }}>
+//               <Card border="secondary">
+//                 <Card.Img variant="top" src={users.images} />
+//                 <Card.Body>
+//                   <Card.Text>{users.username}</Card.Text>
+//                 </Card.Body>
+//               </Card>
+//             </CardDeck>
+//           </div>
+//         );
+//       }
+//     }
+//   })}
+
+//   {/* {users.map((user) => {
+//     {
+//       if (user.images[0] != undefined) {
+//         const img2 = `https://finsta-v2.herokuapp.com/api/users${user.images[0]}`;
+//         console.log(img2);
+//         console.log(user.images[0].url);
+//         console.log(user);
+
+//         return (
+//           <div id="ImageContainer">
+//             <img src={user.images[0].url} alt="" class="Image" />
+//           </div>
+//         );
+//       }
+//     }
+//   })} */}
+// </div>
