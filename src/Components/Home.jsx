8057d9b72acca4357003d31f189506895ce419ae
biprajit163 <biprajit163@gmail.com>
2021-02-09 14:23:18 -0500

    commit deleted files

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -36,4 +36,33 @@ export default function Home() {
 }
 
 
+/*
+
+users.map(item => {
+  if(item.images.length) {
+    let randIndex = Math.floor(Math.random() * item.images.length)
+
+    if(item.images.length < 5) {
+      for(let i=0; i < item.images.length; i++) {
+        return(
+          <div>
+            <img src={item.images[randIndex].url className="Image"}/>
+          </div>
+        )
+      }
+    } else {
+      for(let i=0; i < 5; i++) {
+        return(
+          <div>
+            <img src={item.images[randIndex].url className="Image"}/>
+          </div>
+        )
+      }
+    }
+  } 
+})
+
+*/
+
+
  
